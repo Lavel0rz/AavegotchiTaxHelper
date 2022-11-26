fc2b49d84bccf82e53276b06cb66e3c36c4c6154
Lavel0rz <85870602+Lavel0rz@users.noreply.github.com>
2022-11-26 22:36:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import base64
 from ABI import *
 import json
 
-web3 = Web3(Web3.HTTPProvider(st.secrets('api')))
+web3 = Web3(Web3.HTTPProvider(st.secrets['api']))
 address = '0x86935F11C86623deC8a25696E1C19a8659CbF95d'
 
 contract = web3.eth.contract(address=address, abi=abi)
