959dcfb1a4647002aaf35d7d16e9fa449afba2f1
Lavel0rz <85870602+Lavel0rz@users.noreply.github.com>
2022-12-08 15:55:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from web3 import Web3
 import base64
 from ABI import *
 import json
-
+import pandas as pd
+import numpy as np
+wear_df = pd.read_csv('TESTINGWEAR.csv')
+df_merged = pd.read_csv('TestingGOTCHIS.csv')
+df_ghst = pd.read_csv('GHST.csv')
 web3 = Web3(Web3.HTTPProvider(st.secrets['api']))
 address = '0x86935F11C86623deC8a25696E1C19a8659CbF95d'
 
@@ -20,18 +24,22 @@ def render_svg(svg):
 contract2 = web3.eth.contract(address=address, abi=abi2)
 contract3 = web3.eth.contract(address=address, abi=abi3)
 
-user_address = st.text_input('Input Address', placeholder='0x...')
+user_address = st.text_input('Input Address', placeholder='0x...').lower()
+ls_IDS = list(df_merged[df_merged['Buyer']==user_address.lower()]['ID'].values)
+
+
+
+Gotchid = st.selectbox('Select a Gotcher ID',(ls_IDS))
+
+
+
+Cost = df_merged[df_merged['Buyer']==user_address][df_merged[df_merged['Buyer']==user_address]['ID']==Gotchid]['precio'].values[0]*(df_merged[df_merged['Buyer']==user_address][df_merged[df_merged['Buyer']==user_address]['ID']==Gotchid]['Price'].values[0])
+date = df_merged[df_merged['Buyer']==user_address][df_merged[df_merged['Buyer']==user_address]['ID']==Gotchid]['Date'].values[0]
+
+st.markdown(f'BUY PRICE {round(Cost, 2)}$   \n Date of purchase: {date}')
 try:
-    IDS = (contract3.caller.tokenIdsOfOwner(user_address))
-    col1, col2 = st.columns(2)
-    with col1:
-        for gotchis in IDS[:5]:
-            st.write(contract.caller.getAavegotchiSvg(gotchis),unsafe_allow_html=True)
-    with col2:
-        for gotchis in IDS[5:]:
-            st.write(contract.caller.getAavegotchiSvg(gotchis),unsafe_allow_html=True)
+    sell = df_merged[df_merged['Seller']==user_address][df_merged[df_merged['Seller']==user_address]['ID']==Gotchid]['precio'].values[0]*(df_merged[df_merged['Seller']==user_address][df_merged[df_merged['Seller']==user_address]['ID']==Gotchid]['Price'].values[0])
+    sell_date = df_merged[df_merged['Seller']==user_address][df_merged[df_merged['Seller']==user_address]['ID']==Gotchid]['Date'].values[0]
 
-
-except:
-    st.warning('Insert valid address')
-
+    st.markdown(f'SELL PRICE {round(sell, 2)}$   \n Date of purchase: {sell_date}')
+except:st.warning('No sells found for this GotchiID!')
